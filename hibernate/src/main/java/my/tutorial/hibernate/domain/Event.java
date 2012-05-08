bea9dfa603cf393f642907471c12fec5ae6b1470
Oleksandr Prokopiuk <oleksandr_prokopiuk@epam.com>
2012-05-08 14:17:56 +0300

    add frame of hibernate tutorial

---
+++ hibernate/src/main/java/my/tutorial/hibernate/domain/Event.java
@@ -0,0 +1,34 @@
+package my.tutorial.hibernate.domain;
+
+import java.util.Date;
+
+public class Event {
+	private long id;
+	private Date date;
+	private String title;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
